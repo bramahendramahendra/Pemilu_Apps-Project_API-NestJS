e83d482778b54a83627cd04ea0efe50c81481bd7
bramahendramahendra <bramahendramahendra@gmail.com>
2024-03-13 05:12:17 +0700

    controller provinsi

--- src/provinsi/provinsi.controller.ts
+++ src/provinsi/provinsi.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ProvinsiService } from './provinsi.service';
+import { CreateProvinsiDto } from './dto/create-provinsi.dto';
+import { UpdateProvinsiDto } from './dto/update-provinsi.dto';
 
 @Controller('provinsi')
-export class ProvinsiController {}
+export class ProvinsiController {
+    constructor(private readonly provinsiService: ProvinsiService) { }
+
+    @Post()
+    @UsePipes(new ValidationPipe({ transform: true }))
+    create(@Body() createProvinsiDto: CreateProvinsiDto) {
+        return this.provinsiService.create(createProvinsiDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.provinsiService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: number) {
+        return this.provinsiService.findOne(id).catch((e) => {
+            throw new NotFoundException(e.message);
+        });
+    }
+
+    @Patch(':id')
+    @UsePipes(new ValidationPipe({ transform: true }))
+    update(@Param('id') id: number, @Body() updateProvinsiDto: UpdateProvinsiDto) {
+        return this.provinsiService.update(id, updateProvinsiDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: number) {
+        return this.provinsiService.remove(id);
+    }
+}
